43b1e8ee6d8fa01e1c9cad784f9496b105067e2c
Hoang Vi <hoangvi.forwork@gmail.com>
2024-04-22 00:13:31 +0700

    (REFATORY) [F_001] Update Login Screen with Button Widget

--- lib/screens/authe_screen/csp_login_screen.dart
+++ lib/screens/authe_screen/csp_login_screen.dart
@@ -1,12 +1,13 @@
-// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
 import 'package:case_sync_pro/resource/csp_colors.dart';
 import 'package:case_sync_pro/resource/csp_dimes.dart';
 import 'package:case_sync_pro/resource/csp_drawable.dart';
 import 'package:case_sync_pro/resource/csp_styles.dart';
 import 'package:case_sync_pro/utils/ui_utils.dart';
+import 'package:case_sync_pro/widgets/csp_text_form_field.dart';
 import 'package:flutter/material.dart';
-
 import '../../resource/csp_lang.dart';
+import '../../widgets/csp_button_widget.dart';
 
 class CSPLoginScreen extends StatelessWidget {
   @override
@@ -22,42 +23,47 @@ class CSPLoginScreen extends StatelessWidget {
           height: CSPDimens.w50 + 6,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(CSPDimens.margin12),
+      body: Container(
+        padding: EdgeInsets.all(CSPDimens.margin12),
+        margin: EdgeInsets.only(top: CSPDimens.margin100),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             UiUtil.verticalSpace50,
-            const TextFormFieldAndTitle(title: CSPLanguage.enterEmail),
-            UiUtil.verticalSpace30,
-            const TextFormFieldAndTitle(title: CSPLanguage.enterPass),
-            UiUtil.verticalSpace16,
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                CSPLanguage.forgotPassword,
-                style: CSPTextStyle.textStyleNormal.copyWith(
-                  color: Colors.blueAccent,
-                  fontStyle: FontStyle.italic,
-                ),
-              ),
+            CSPTextFormField(
+              hintText: CSPLanguage.enterEmail,
+              hintStyle: CSPTextStyle.textStyleSmall,
             ),
             UiUtil.verticalSpace30,
-            ElevatedButton(
-              onPressed: () {},
-              child: Center(
-                child: Text(
-                  CSPLanguage.login,
-                  style: CSPTextStyle.textStyleMedium,
-                ),
-              ),
+            CSPTextFormField(
+              hintText: CSPLanguage.enterPass,
+              hintStyle: CSPTextStyle.textStyleSmall,
+              suffixWidget: const Icon(Icons.remove_red_eye),
             ),
-            UiUtil.verticalSpace16,
-            OutlinedButton(
+            UiUtil.verticalSpace8,
+            CSPButtonWidget(
+              type: CSPAppButtonType.linkText,
+              disable: false,
+              fullWidth: false,
+              isLoading: false,
+              label: CSPLanguage.forgotPassword,
+            ),
+            UiUtil.verticalSpace30,
+            CSPButtonWidget(
+              type: CSPAppButtonType.primary,
+              disable: false,
+              fullWidth: true,
+              isLoading: false,
+              label: CSPLanguage.login,
               onPressed: () {},
-              child: const Center(
-                child: Text(CSPLanguage.register),
-              ),
+            ),
+            UiUtil.verticalSpace8,
+            CSPButtonWidget(
+              type: CSPAppButtonType.linkText,
+              disable: false,
+              fullWidth: true,
+              isLoading: false,
+              label: CSPLanguage.registerQuestion,
             ),
           ],
         ),
